d530183a927626c67546cc2311d488e18d717540
warrenulrich <warren.ulrich@outlook.com>
2023-02-03 22:55:34 -0800

    sleep_for, sleep_until node_task routines added

---
+++ include/bt/sleep.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <chrono>
+
+#include <bt/node_task.hpp>
+#include <bt/yield.hpp>
+
+namespace bt {
+  template <typename Rep, typename Period>
+  node_task sleep_for(std::chrono::duration<Rep, Period> duration) {
+    const auto now = std::chrono::steady_clock::now();
+    const auto end = now + duration;
+
+    while (std::chrono::steady_clock::now() < end)
+      co_await yield();
+    
+    co_return node_status::success;
+  }
+
+  template <typename Clock, typename Duration>
+  node_task sleep_until(std::chrono::time_point<Clock, Duration> time_point) {
+    while (std::chrono::steady_clock::now() < time_point)
+      co_await yield();
+    
+    co_return node_status::success;
+  }
+
+  template <typename Predicate>
+  node_task sleep_until(Predicate&& predicate) {
+    while (!predicate())
+      co_await yield();
+    
+    co_return node_status::success;
+  }
+}
